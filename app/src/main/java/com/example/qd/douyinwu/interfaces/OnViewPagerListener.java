3b3baca786566ce30deb1e834c4c68f61a9bc9d2
Fuge2008 <fzwooo@163.com>
2019-04-28 03:20:41 +0800

    初始化代码-fuzw  2019-04-28  03:20

---
+++ app/src/main/java/com/example/qd/douyinwu/interfaces/OnViewPagerListener.java
@@ -0,0 +1,15 @@
+package com.example.qd.douyinwu.interfaces;
+
+import android.view.View;
+
+public interface OnViewPagerListener {
+
+
+        void onInitComplete(View view);
+
+        void onPageRelease(boolean isNext, int position, View view);
+
+        void onPageSelected(int position, boolean isBottom, View view);
+
+
+}
