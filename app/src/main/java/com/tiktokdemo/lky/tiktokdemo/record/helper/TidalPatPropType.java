5fe1f5ecd889cbd2d2f61350a3e74ea220efc388
Fuge2008 <fzwooo@163.com>
2019-04-29 14:15:14 +0800

    增加美颜模块.----fuzw 2019-04-29 14:14

---
+++ app/src/main/java/com/tiktokdemo/lky/tiktokdemo/record/helper/TidalPatPropType.java
@@ -0,0 +1,27 @@
+package com.tiktokdemo.lky.tiktokdemo.record.helper;
+
+
+import com.example.qd.douyinwu.R;
+
+/**
+ * Created by lky on 2017/5/3.
+ */
+
+public enum  TidalPatPropType {
+    DEFAULT(R.mipmap.chaopai_luzhi_wudaoju),
+    CIGAR(R.mipmap.icon_cigar),
+    GLASSES(R.mipmap.icon_glasses),
+    FACECLOTH(R.mipmap.icon_towel),
+    GROUP1(R.mipmap.icon_group2),
+    GROUP2(R.mipmap.icon_group1);
+
+
+    private int mBackgroundRes;
+    TidalPatPropType(int backgroundRes) {
+        this.mBackgroundRes = backgroundRes;
+    }
+
+    public int getBackgroundRes() {
+        return mBackgroundRes;
+    }
+}
